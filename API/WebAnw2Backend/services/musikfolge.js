ef2f79e802ec0381c96b6c7b8cc89d79b0dbc5c8
Vogt Janina MTA <janina.vogt@mt.com>
2022-12-05 17:27:37 +0100

    Fixed Bug

--- API/WebAnw2Backend/services/musikfolge.js
+++ API/WebAnw2Backend/services/musikfolge.js
@@ -1,80 +1,80 @@
 const helper = require('../helper.js');
-const BearbeiterDao = require('../dao/bearbeiterDao.js');
+const MusikfolgeDao = require('../dao/musikfolgeDao.js');
 const express = require('express');
 var serviceRouter = express.Router();
 
-console.log('- Service Bearbeiter');
+console.log('- Service Musikfolge');
 
-serviceRouter.get('/bearbeiter/gib/:id', function(request, response) {
-    console.log('Service Bearbeiter: Client requested one record, id=' + request.params.id);
+serviceRouter.get('/musikfolge/gib/:id', function(request, response) {
+    console.log('Service Musikfolge: Client requested one record, id=' + request.params.id);
 
-    const bearbeiterDao = new BearbeiterDao(request.app.locals.dbConnection);
+    const musikfolgeDao = new MusikfolgeDao(request.app.locals.dbConnection);
     try {
-        var obj = bearbeiterDao.loadById(request.params.id);
-        console.log('Service Bearbeiter: Record loaded');
+        var obj = musikfolgeDao.loadById(request.params.id);
+        console.log('Service Musikfolge: Record loaded');
         response.status(200).json(obj);
     } catch (ex) {
-        console.error('Service Bearbeiter: Error loading record by id. Exception occured: ' + ex.message);
+        console.error('Service Musikfolge: Error loading record by id. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
 
-serviceRouter.get('/bearbeiter/alle', function(request, response) {
-    console.log('Service Bearbeiter: Client requested all records');
+serviceRouter.get('/musikfolge/alle', function(request, response) {
+    console.log('Service Musikfolge: Client requested all records');
 
-    const bearbeiterDao = new BearbeiterDao(request.app.locals.dbConnection);
+    const musikfolgeDao = new MusikfolgeDao(request.app.locals.dbConnection);
     try {
-        var arr = bearbeiterDao.loadAll();
-        console.log('Service Bearbeiter: Records loaded, count=' + arr.length);
+        var arr = musikfolgeDao.loadAll();
+        console.log('Service Musikfolge: Records loaded, count=' + arr.length);
         response.status(200).json(arr);
     } catch (ex) {
-        console.error('Service Bearbeiter: Error loading all records. Exception occured: ' + ex.message);
+        console.error('Service Musikfolge: Error loading all records. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
 
-serviceRouter.get('/bearbeiter/existiert/:id', function(request, response) {
-    console.log('Service Bearbeiter: Client requested check, if record exists, id=' + request.params.id);
+serviceRouter.get('/musikfolge/existiert/:id', function(request, response) {
+    console.log('Service Musikfolge: Client requested check, if record exists, id=' + request.params.id);
 
     console.log('go');
 
-    const bearbeiterDao = new BearbeiterDao(request.app.locals.dbConnection);
+    const musikfolgeDao = new MusikfolgeDao(request.app.locals.dbConnection);
     try {
-        var exists = bearbeiterDao.exists(request.params.id);
-        console.log('Service Bearbeiter: Check if record exists by id=' + request.params.id + ', exists=' + exists);
+        var exists = musikfolgeDao.exists(request.params.id);
+        console.log('Service Musikfolge: Check if record exists by id=' + request.params.id + ', exists=' + exists);
         response.status(200).json({'id': request.params.id, 'existiert': exists});
     } catch (ex) {
-        console.error('Service Bearbeiter: Error checking if record exists. Exception occured: ' + ex.message);
+        console.error('Service Musikfolge: Error checking if record exists. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
 
-serviceRouter.post('/bearbeiter', function(request, response) {
-    console.log('Service Bearbeiter: Client requested creation of new record');
+serviceRouter.post('/musikfolge', function(request, response) {
+    console.log('Service Musikfolge: Client requested creation of new record');
 
     var errorMsgs=[];
     if (helper.isUndefined(request.body.name)) 
         errorMsgs.push('name fehlt');
     
     if (errorMsgs.length > 0) {
-        console.log('Service Bearbeiter: Creation not possible, data missing');
+        console.log('Service Musikfolge: Creation not possible, data missing');
         response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
         return;
     }
 
-    const bearbeiterDao = new BearbeiterDao(request.app.locals.dbConnection);
+    const musikfolgeDao = new MusikfolgeDao(request.app.locals.dbConnection);
     try {
-        var obj = bearbeiterDao.create(request.body.name);
-        console.log('Service Bearbeiter: Record inserted');
+        var obj = musikfolgeDao.create(request.body.name);
+        console.log('Service Musikfolge: Record inserted');
         response.status(200).json(obj);
     } catch (ex) {
-        console.error('Service Bearbeiter: Error creating new record. Exception occured: ' + ex.message);
+        console.error('Service Musikfolge: Error creating new record. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }    
 });
 
-serviceRouter.put('/bearbeiter', function(request, response) {
-    console.log('Service Bearbeiter: Client requested update of existing record');
+serviceRouter.put('/musikfolge', function(request, response) {
+    console.log('Service Musikfolge: Client requested update of existing record');
 
     var errorMsgs=[];
     if (helper.isUndefined(request.body.id)) 
@@ -83,33 +83,33 @@ serviceRouter.put('/bearbeiter', function(request, response) {
         errorMsgs.push('name fehlt');
 
     if (errorMsgs.length > 0) {
-        console.log('Service Bearbeiter: Update not possible, data missing');
+        console.log('Service Musikfolge: Update not possible, data missing');
         response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
         return;
     }
 
-    const bearbeiterDao = new BearbeiterDao(request.app.locals.dbConnection);
+    const musikfolgeDao = new MusikfolgeDao(request.app.locals.dbConnection);
     try {
-        var obj = bearbeiterDao.update(request.body.id, request.body.name);
-        console.log('Service Bearbeiter: Record updated, id=' + request.body.id);
+        var obj = musikfolgeDao.update(request.body.id, request.body.name);
+        console.log('Service Musikfolge: Record updated, id=' + request.body.id);
         response.status(200).json(obj);
     } catch (ex) {
-        console.error('Service Bearbeiter: Error updating record by id. Exception occured: ' + ex.message);
+        console.error('Service Musikfolge: Error updating record by id. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }    
 });
 
-serviceRouter.delete('/bearbeiter/:id', function(request, response) {
-    console.log('Service Bearbeiter: Client requested deletion of record, id=' + request.params.id);
+serviceRouter.delete('/musikfolge/:id', function(request, response) {
+    console.log('Service Musikfolge: Client requested deletion of record, id=' + request.params.id);
 
-    const bearbeiterDao = new BearbeiterDao(request.app.locals.dbConnection);
+    const musikfolgeDao = new MusikfolgeDao(request.app.locals.dbConnection);
     try {
-        var obj = bearbeiterDao.loadById(request.params.id);
-        bearbeiterDao.delete(request.params.id);
-        console.log('Service Bearbeiter: Deletion of record successfull, id=' + request.params.id);
+        var obj = musikfolgeDao.loadById(request.params.id);
+        musikfolgeDao.delete(request.params.id);
+        console.log('Service Musikfolge: Deletion of record successfull, id=' + request.params.id);
         response.status(200).json({ 'gelöscht': true, 'eintrag': obj });
     } catch (ex) {
-        console.error('Service Bearbeiter: Error deleting record. Exception occured: ' + ex.message);
+        console.error('Service Musikfolge: Error deleting record. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
