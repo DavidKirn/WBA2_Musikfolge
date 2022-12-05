ef6b0752c8da56230eeda108c6d1871db229bc87
Vogt Janina MTA <janina.vogt@mt.com>
2022-12-05 17:03:11 +0100

    Musikfolge (Dao und Service) hinzugefügt

---
+++ API/WebAnw2Backend/dao/musikfolgeDao.js
@@ -0,0 +1,90 @@
+const helper = require('../helper.js');
+
+class MusikfolgeDao {
+
+    constructor(dbConnection) {
+        this._conn = dbConnection;
+    }
+
+    getConnection() {
+        return this._conn;
+    }
+
+    loadById(id) {
+        var sql = 'SELECT * FROM Musikfolge WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
+
+        if (helper.isUndefined(result)) 
+            throw new Error('No Record found by id=' + id);
+
+        return result;
+    }
+
+    loadAll() {
+        var sql = 'SELECT * FROM Musikfolge';
+        var statement = this._conn.prepare(sql);
+        var result = statement.all();
+
+        if (helper.isArrayEmpty(result)) 
+            return [];
+        
+        return result;
+    }
+
+    exists(id) {
+        var sql = 'SELECT COUNT(id) AS cnt FROM Musikfolge WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
+
+        if (result.cnt == 1) 
+            return true;
+
+        return false;
+    }
+
+    create(vereinsid=0, veranstaltungsid=0) {
+        var sql = 'INSERT INTO Musikfolge (vereinsid, veranstaltungsid) VALUES (?,?)';
+        var statement = this._conn.prepare(sql);
+        var params = [vereinsid, veranstaltungsid];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not insert new Record. Data: ' + params);
+
+        return this.loadById(result.lastInsertRowid);
+    }
+
+    update(id, vereinsid=0, veranstaltungsid=0) {
+        var sql = 'UPDATE Musikfolge SET vereinsid=?, veranstaltungsid=? WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var params = [vereinsid, veranstaltungsid, id];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not update existing Record. Data: ' + params);
+
+        return this.loadById(id);
+    }
+
+    delete(id) {
+        try {
+            var sql = 'DELETE FROM Musikfolge WHERE id=?';
+            var statement = this._conn.prepare(sql);
+            var result = statement.run(id);
+
+            if (result.changes != 1) 
+                throw new Error('Could not delete Record by id=' + id);
+
+            return true;
+        } catch (ex) {
+            throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message);
+        }
+    }
+
+    toString() {
+        console.log('MusikfolgeDao [_conn=' + this._conn + ']');
+    }
+}
+
+module.exports = MusikfolgeDao;
